9a709b9a6eaf7bf9077bc23192a902d3e338c2ea
Justice <justice.addico.ja@gmail.com>
2021-06-21 08:00:17 +0000

    Initial heroku config

--- src/data/helpers/settings.ts
+++ src/data/helpers/settings.ts
@@ -1,9 +1,10 @@
 const data = {
     localUrl: "http://localhost:5000",
+    onlineUrl:"https://razorgripassignmentapi.azurewebsites.net"
 }
 
 export const settings = {
-    baseUri : data.localUrl,
+    baseUri : data.onlineUrl,
     loader : "assets/images/transparentloader.png",
     defaultUserImage:"assets/svgs/defaultuser.svg"
 }
